7808adaf92398a42094836c87c6db1c2b232b7d3
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-09-05 03:00:29 -0700

    Add PSGI::Util::foreach

---
+++ lib/PSGI/Util.pm
@@ -0,0 +1,30 @@
+package PSGI::Util;
+
+# Is it safe to use Scalar::Util everywhere?
+sub _blessed {
+    ref $_[0] && ref $_[0] !~ /^(?:SCALAR|ARRAY|HASH|CODE|GLOB|Regexp)$/;
+}
+
+sub foreach {
+    my($body, $cb) = @_;
+
+    if (ref $body eq 'ARRAY') {
+        for my $line (@$body) {
+            $cb->($line);
+        }
+    } elsif (ref $body eq 'GLOB' || (_blessed($body) && $body->can('getline'))) {
+        while (defined(my $line = $body->getline)) {
+            $cb->($line);
+        }
+        $body->close if $body->can('close');
+    } elsif (ref $body eq 'CODE') {
+        while (defined(my $line = $body->())) {
+            $cb->($line);
+        }
+    } else {
+        $body->foreach($cb);
+        $body->close if $body->can('close');
+    }
+}
+
+1;
